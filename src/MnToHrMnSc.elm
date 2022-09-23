5ab617423d1a00e4aa0025cf0f5a26b67b97411c
Polarit <jjlammi@yahoo.com>
2022-09-23 21:58:33 +0300

    format

--- src/MnToHrMnSc.elm
+++ src/MnToHrMnSc.elm
@@ -1,17 +1,28 @@
 module MnToHrMnSc exposing (mnToHrMn)
 
-
 -- Converts minutes to hh:mm:ss
 -- also negative minutes, example -10 -> 23:50
 -- also decimal part to seconds, example mnToHrMn 1435.25 -> 23:55:15
 
+
 mnToHrMn : Float -> String
 mnToHrMn mns =
-    let mna = if (mns >= 0) then mns
-              else 1440 + mns
-        lmins = remainderBy 60 (floor mna)
-        lhrs = floor ( mna / 60.0 )
-        lsec = floor (60*mna - toFloat(3600*lhrs + 60*lmins ))
+    let
+        mna =
+            if mns >= 0 then
+                mns
+
+            else
+                1440 + mns
+
+        lmins =
+            remainderBy 60 (floor mna)
+
+        lhrs =
+            floor (mna / 60.0)
+
+        lsec =
+            floor (60 * mna - toFloat (3600 * lhrs + 60 * lmins))
     in
     zeroFill lhrs
         ++ ":"
@@ -19,9 +30,11 @@ mnToHrMn mns =
         ++ ":"
         ++ zeroFill lsec
 
+
 zeroFill : Int -> String
 zeroFill x =
     if x < 10 then
         "0" ++ String.fromInt x
+
     else
         String.fromInt x
