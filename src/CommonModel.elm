48d0dc91f42fb0ddcf44682b4195ba65ff12de18
Polarit <jjlammi@yahoo.com>
2022-09-24 23:24:08 +0300

    reused model

---
+++ src/CommonModel.elm
@@ -0,0 +1,16 @@
+module CommonModel exposing (..)
+
+-- MODEL
+
+
+type alias InputData =
+    { year : String
+    , month : String
+    , day : String
+    , hour : String
+    , minute : String
+    , second : String
+    , latitude : String
+    , longitude : String
+    , timezone : String
+    }
