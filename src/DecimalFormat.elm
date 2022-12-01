d12fd03510ee18eb23be1f1b64e6b0010957791a
Polarit <jarmol@users.noreply.github.com>
2022-12-01 17:23:27 +0200

    US local format numbers

--- src/DecimalFormat.elm
+++ src/DecimalFormat.elm
@@ -1,26 +1,25 @@
 module DecimalFormat exposing (cutDec3, cutDec6)
 
 import FormatNumber exposing (format)
-import FormatNumber.Locales exposing (Decimals(..), frenchLocale)
-
+import FormatNumber.Locales exposing (Decimals(..), Locale, usLocale)
 
 sharesLocale =
-    { frenchLocale
-        | decimals = Exact 6
+    { usLocale
+       | decimals = Exact 6
         , negativePrefix = "-"
         , positivePrefix = " "
+        
     }
-
+    
 
 sharesLocale3 =
     { sharesLocale
         | decimals = Exact 3
     }
 
-
 cutDec6 x =
-    format sharesLocale x
-
+   format sharesLocale x
 
 cutDec3 x =
-    format sharesLocale3 x
+   format sharesLocale3 x
+
