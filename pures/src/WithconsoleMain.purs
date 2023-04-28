d22a6187a04682703b1b486776bfaa53c48e222d
Polarit <jarmol@users.noreply.github.com>
2023-04-28 18:57:30 +0300

    Use with output to console - no html

---
+++ pures/src/WithconsoleMain.purs
@@ -0,0 +1,25 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log, logShow)
+import TryPureScript (render, withConsole)
+import Data.Number (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+
+circleArea :: Number -> Number
+circleArea r =
+  pi * r * r
+  
+
+main :: Effect Unit
+main = render =<< withConsole do
+  log $ "sqrt 2.0 = "
+  logShow $ sqrt 2.0
+  log "Hypotenuse"
+  logShow $ diagonal 3.0 4.0
+  log "Area of circle with radius 5"
+  logShow $ circleArea 5.0
