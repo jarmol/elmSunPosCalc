9701c5339b3af45f3266644b4daf860266006266
Polarit <jarmol@users.noreply.github.com>
2023-04-28 19:30:55 +0300

    Update WithconsoleMain.purs

--- pures/src/WithconsoleMain.purs
+++ pures/src/WithconsoleMain.purs
@@ -17,9 +17,6 @@ circleArea r =
 
 main :: Effect Unit
 main = render =<< withConsole do
-  log $ "sqrt 2.0 = "
-  logShow $ sqrt 2.0
-  log "Hypotenuse"
-  logShow $ diagonal 3.0 4.0
-  log "Area of circle with radius 5"
-  logShow $ circleArea 5.0
+  log ("sqrt 2.0 = " <>  show (sqrt 2.0))
+  log ("Hypotenuse = " <> show (diagonal 3.0 4.0))
+  log ("Area of circle with radius 10 = " <> show (circleArea 10.0))
