98009b5b0d0a15236637d0a004740f84c975bafd
Polarit <jarmol@users.noreply.github.com>
2023-05-02 23:20:21 +0300

    Calculating JDN and weekday from the given date

--- pures/src/test-julian.purs
+++ pures/src/test-julian.purs
@@ -25,23 +25,66 @@ negdiv :: Int -> Int -> Int
 negdiv n  m = if n < 0 then -(div (-n) m)
               else div n m
 
-main:: Effect Unit
-main = render =<< withConsole do
+
+weekday :: Int -> Int -> Int -> String
+weekday y m d =
+    let
+        dayNumber =
+            mod (1 + jdnGr y m d) 7
+    in
+    case dayNumber of
+        0 ->
+            "Sunday"
+        1 ->
+            "Monday"
+        2 ->
+            "Tuesday"
+        3 ->
+            "Wednesday"
+        4 ->
+            "Thursday"
+        5 ->
+            "Friday"
+        6 ->
+            "Saturday"
+        _ -> 
+            "undefined"
+
+
+
+main :: Effect Unit
+main  =
+  render =<< withConsole do
   log $  ("TESTING JULIAN DATE NUMBERS 'JDN' AND INFIX OPERATOR '//'")
   log $  ("div 13 2 = "   <> show ( div 13 2 == 6 ))
   log $  ("13 // 2 = "    <> show ( 13 // 2 == 6 ))
   log $  ("-13 // 2 = "   <> show ( -13 // 2 == -6 ))
-  log$   ("-12 // 12 = "  <> show ( -12 // 12 == -1 ))
-  log$   ("12 // 12 = "   <> show ( 12 // 12 == 1 ))
+  log $  ("-12 // 12 = "  <> show ( -12 // 12 == -1 ))
+  log $  ("12 // 12 = "   <> show ( 12 // 12 == 1 ))
   log $  ("negdiv 13 2 = "    <> show ( negdiv 13 2 == 6 ))
   log $  ("negdiv (-13) 2 = " <> show ( negdiv (-13) 2 == -6 ))
-  log $  ("2023-02-13 JDN = " <> show ( jdnGr 2023 2 13))
-  log $  ("2023-02-24 JDN = " <> show ( jdnGr 2023 2 24))
-  log $  ("2023-02-28 JDN = " <> show ( jdnGr 2023 2 28))
-  log $  ("2023-03-01 JDN = " <> show ( jdnGr 2023 3 1))
-  log $  ("2020-02-29 JDN = " <> show ( jdnGr 2020 2 29))
-  log $  ("2020-03-01 JDN = " <> show ( jdnGr 2020 3 1))
+  log $  (printAB 2023 2 24)
+  log $  (printAB 2020 2 28)
+  log $  (printAB 2023 3 1)
+  log $  (printAB 2020 2 29)
+  log $  (printAB 2020 3 1)  
+  log $  ("Weekday 2023-04-30 = ") <> ( weekday 2023 4 30 )
+  log $  ("Weekday 2023-05-01 = ") <> ( weekday 2023 5 1 )
+  log $  ("Weekday 2023-05-02 = ") <> ( weekday 2023 5 2 )
+
   
+printA :: Int -> Int -> Int -> String
+printA j m t =
+      show j <> "-" <> show m <> "-" <> show t
+
+printB :: Int -> Int -> Int -> String
+printB j m t =
+      " JDN = " <> show (jdnGr j m t)
+
+printAB ::  Int -> Int -> Int -> String
+printAB j m t =
+  (printA j m t) <> (printB j m t)
+
 -- testing
 -- main = jdnGr 2023 2 13  -- 2459989
 
