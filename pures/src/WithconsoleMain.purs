91eb3bfb5ba10df92e15006749753066891f505b
Polarit <jarmol@users.noreply.github.com>
2023-05-17 14:10:34 +0300

    updated

--- pures/src/WithconsoleMain.purs
+++ pures/src/WithconsoleMain.purs
@@ -4,7 +4,8 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import TryPureScript (render, withConsole)
-import Data.Number (sqrt, pi, sin, cos, round)
+import Data.Number (sqrt, pi, sin, cos)
+import Data.Number.Format (toStringWith, fixed, exponential)
 
 diagonal :: Number -> Number -> Number
 diagonal = \w h ->  sqrt (w * w + h * h)
@@ -19,24 +20,23 @@ cosDeg = \d -> cos (pi * d / 180.0)
 sinDeg :: Number -> Number
 sinDeg = \d -> sin (pi * d / 180.0)
 
-fixTo6Decim :: Number -> Number
-fixTo6Decim x = 1.0e-7 * round (1.0e7*x)
+s60 = toStringWith (fixed 4) ( cosDeg 60.0 ) :: String
+s30 = toStringWith (fixed 4) ( sinDeg 30.0 ) :: String
 
 main :: Effect Unit
 main = render =<< withConsole do
-  log $  ("sqrt 2.0 = "   <> show ( sqrt 2.0 ))
-  log $  ("Hypotenuse = " <> show ( diagonal 3.0 4.0 ))
-  log $  ("Area of circle with radius 10 = "
-  <> show ( circleArea 10.0 ))
-  log $  ("cos 60° = " <> show ( cosDeg 60.0 ))
-  log $  ("sin 30° = " <> show ( sinDeg 30.0 ))
-  log $  ("sin 30° is equal to cos 60°, "
-  <> show (cosDeg 60.0 == sinDeg 30.0))
-  log $  ("Difference cos 60° - sin 30 = "
-  <> show (cosDeg 60.0 - sinDeg 30.0))
-  log $  ("sin 30° rounded = "
-  <> show (fixTo6Decim (sinDeg 30.0)))
-  log $  ("cos 60° rounded = "
-  <> show (fixTo6Decim (cosDeg 60.0)))
-  log $  ("Rounded sin 30° is equal to rounded cos 60°, "
-  <> show (fixTo6Decim (cosDeg 60.0) == fixTo6Decim (sinDeg 30.0)))
+  log $  "NUMBER FORMAT"
+  log $  "sqrt 2.0 ≅ "   <> toStringWith (fixed 6) ( sqrt 2.0 )
+  log $  "Hypotenuse of rectangled triangle with catheti 3 and 4 = "
+    <> show ( diagonal 3.0 4.0 )
+  log $  "Area of circle with radius 10 ≅ "
+    <> toStringWith (fixed 6) ( circleArea 10.0 )
+  log $  "cos 60° = " <> s60
+  log $  "sin 30° = " <> s30
+  log $  "sin 30° is equal to cos 60°, "
+    <> show (cosDeg 60.0 == sinDeg 30.0)
+  log $  "Difference of values cos 60° and sin 30 ≅ "
+    <> toStringWith (exponential 4) (cosDeg 60.0 - sinDeg 30.0)
+  log $ ("Comparing fixed values as strings, claim these are equal is "
+    <> show (s60 == s30))
+
