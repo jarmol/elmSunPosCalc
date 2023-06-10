262fbb73042b41625dcadcb6b03422409923756e
Polarit <jarmol@users.noreply.github.com>
2023-06-10 15:04:53 +0300

    infinite tan value included

--- pures/src/arrayfuncs.purs
+++ pures/src/arrayfuncs.purs
@@ -5,7 +5,7 @@ import Data.Array (filter, range)
 import Data.Foldable (foldl)
 import Effect (Effect)
 import Effect.Console (logShow, log)
-import Data.Number (sin, cos, pi)
+import Data.Number (sin, cos, tan, pi, isFinite)
 import Data.Number.Format (toStringWith, fixed)
 
 import TryPureScript (render, withConsole)
@@ -31,11 +31,17 @@ sinvals = map (\n -> sin (pi / n) ) decar
 cosvals :: Array Number
 cosvals = map (\n -> cos (pi / n) ) decar
 
+tanvals :: Array Number
+tanvals = map (\n -> tan (pi / n) ) decar
+
 fixsinvals :: Array String
 fixsinvals = map (\n -> toStringWith (fixed 3) n) sinvals
 
 fixcosvals :: Array String
 fixcosvals = map (\n -> toStringWith (fixed 3) n) cosvals
+
+fixtanvals :: Array String
+fixtanvals = map (\n -> if (n < 1.0e16) then toStringWith (fixed 3) n else "∞") tanvals
 
 inverts :: Array Number
 inverts = [2.0, 4.0, 5.0, 10.0] <#> \n -> 1.0 / n 
@@ -57,8 +63,9 @@ main = render =<< withConsole do
   log $ "Inverted " <> show inverts
   log $ "π/n in grades " <> show pipern
   log $ "π/n table " <> show pidivs
-  log $ "sin(π/n) table " <> show fixsinvals
-  log $ "cos(π/n) table " <> show fixcosvals
+  log $ "sin(π/n) array " <> show fixsinvals
+  log $ "cos(π/n) array " <> show fixcosvals
+  log $ "tan(π/n) array " <> show fixtanvals
   log $ "Even true or false " <> show evens
   log $ "Even only " <> show evenonly
   log $ "Sum of array cells " <> show (foldl (+) 0 ar10)
